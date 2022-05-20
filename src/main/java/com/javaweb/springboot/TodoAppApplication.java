afc599e712bf9bc373b2fb3eceb1c22838c6e9fb
Dang Kiet Nguyen <kietnguyen17052001@gmail.com>
2022-05-20 18:58:13 +0700

    add modelMapper()

--- src/main/java/com/javaweb/springboot/TodoAppApplication.java
+++ src/main/java/com/javaweb/springboot/TodoAppApplication.java
@@ -1,11 +1,17 @@
 package com.javaweb.springboot;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class TodoAppApplication {
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(TodoAppApplication.class, args);
