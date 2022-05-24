879af6711e57a6de7265c144a1b392b4a6267b64
Dang Kiet Nguyen <kietnguyen17052001@gmail.com>
2022-05-24 14:47:00 +0700

    mofidied api user

--- src/main/java/com/javaweb/springboot/controllers/UserController.java
+++ src/main/java/com/javaweb/springboot/controllers/UserController.java
@@ -2,6 +2,7 @@ package com.javaweb.springboot.controllers;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,6 +17,7 @@ import com.javaweb.springboot.services.UserService;
 
 @RestController
 @RequestMapping("/api/users")
+@CrossOrigin
 public class UserController {
 	@Autowired
 	private UserService service;
