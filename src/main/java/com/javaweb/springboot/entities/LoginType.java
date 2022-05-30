a53929b39bd3ccc7064c8189d3568de21ba0f9b0
Dang Kiet Nguyen <kietnguyen17052001@gmail.com>
2022-05-31 00:22:40 +0700

    create LoginType object

---
+++ src/main/java/com/javaweb/springboot/entities/LoginType.java
@@ -0,0 +1,45 @@
+package com.javaweb.springboot.entities;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tbl_logintype")
+public class LoginType {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column
+	private int id;
+	@Column
+	private String name;
+	@OneToMany(mappedBy = "loginType")
+	private List<User> users;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public List<User> getUsers() {
+		return users;
+	}
+
+	public void setUsers(List<User> users) {
+		this.users = users;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+}
