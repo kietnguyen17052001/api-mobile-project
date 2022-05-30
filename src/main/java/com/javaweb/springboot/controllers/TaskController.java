d0c52802be2d41e43056b0f35dcb9cc361522a89
Dang Kiet Nguyen <kietnguyen17052001@gmail.com>
2022-05-30 08:54:49 +0700

    add cross anotation

--- src/main/java/com/javaweb/springboot/controllers/TaskController.java
+++ src/main/java/com/javaweb/springboot/controllers/TaskController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,6 +22,7 @@ import com.javaweb.springboot.services.TaskService;
 
 @RestController
 @RequestMapping("/api/users")
+@CrossOrigin
 public class TaskController {
 	@Autowired
 	private TaskService service;
